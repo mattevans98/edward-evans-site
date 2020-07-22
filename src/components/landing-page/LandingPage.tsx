90b2c72bde7888f1ca4f787288a3e834c906126b
Matt Evans <mattevans98@gmail.com>
2020-07-22 01:29:51 -0500

    Improved the fade in functionality

--- src/components/landing-page/LandingPage.tsx
+++ src/components/landing-page/LandingPage.tsx
@@ -1,28 +1,45 @@
 import React, { useEffect, useState } from 'react';
-import { Typography, Box, Fade } from '@material-ui/core';
+import { Typography, Box, Fade, Button } from '@material-ui/core';
+import { FadeInModel } from './utils/LandingPage.model';
 
 const LandingPage: React.FC = () => {
-	const [quoteIn, setQuoteIn] = useState(false);
-	const [nameIn, setNameIn] = useState(false);
+	const [isFadedIn, setIsFadedIn] = useState<FadeInModel>({
+		quote: false,
+		attribution: false,
+		enterButton: false
+	});
+	const [fadeInStep, setFadeInStep] = useState<number>(0);
 
-	const fadeInCounter = (): void => {
-		setTimeout(() => setQuoteIn(true), 1000);
-		setTimeout(() => setNameIn(true), 3500);
-	};
-
-	useEffect((): void => fadeInCounter());
+	useEffect((): void => {
+		const fadeInTimer = setInterval((): void => {
+			setIsFadedIn((prevState) => {
+				let newVals = {
+					...prevState
+				};
+				newVals[Object.keys(prevState)[fadeInStep]] = true;
+				return newVals;
+			});
+			setFadeInStep((prevState) => prevState + 1);
+		}, 1500);
+		if (fadeInStep === 3) {
+			clearInterval(fadeInTimer);
+		}
+	});
 
 	return (
 		<Box className="landing-box">
-			<Fade in={quoteIn} timeout={2000}>
+			<Fade in={isFadedIn.quote} timeout={2000}>
 				<Typography variant="h1" className="landing-header">
 					"So you’ve got to love it and you’ve got to have passion and I think that’s the high-order bit."
 				</Typography>
 			</Fade>
-			<Fade in={nameIn} timeout={2000}>
+			<Fade in={isFadedIn.attribution} timeout={2000}>
 				<Typography variant="h3" className="landing-header">
 					-Steve Jobs
 				</Typography>
+			</Fade>
+			<Fade in={isFadedIn.enterButton} timeout={2000}>
+				<Button className="landing-button">Enter Site</Button>
 			</Fade>
 		</Box>
 	);
