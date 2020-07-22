60a621546f9be283e9e227e83f1351cf26e860cb
Matt Evans <mattevans98@gmail.com>
2020-07-22 00:18:55 -0500

    Added color palette to SCSS

---
+++ src/components/homepage/HomePage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Typography, Box } from '@material-ui/core';
+
+const HomePage: React.FC = () => {
+	return (
+		<Box className="container">
+			<Typography variant="body1" className="body-text">
+				High-Order Bit, LLC.
+			</Typography>
+		</Box>
+	);
+};
+
+export default HomePage;
