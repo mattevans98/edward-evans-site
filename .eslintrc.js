a57cdc67be73aab2e41e2ad7b6e3f28f8c50bb71
Matt Evans <mattevans98@gmail.com>
2022-11-11 11:43:48 -0600

    Fixed .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,5 @@
 module.exports = {
 	parser: '@typescript-eslint/parser', // Specifies the ESLint parser
-	extends: [
-		'plugin:prettier/recommended',
-		'plugin:cypress/recommended'
-	],
 	parserOptions: {
 		ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
 		sourceType: 'module' // Allows for the use of imports
