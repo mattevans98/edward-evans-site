29f1f982ba1806553fde1122dcc973b7ec69e1cc
Matt Evans <mattevans98@gmail.com>
2022-11-11 12:01:59 -0600

    Added colors.ts to remove hardcoded color values

--- src/styles/components/common/NavMenu.style.ts
+++ src/styles/components/common/NavMenu.style.ts
@@ -1,13 +1,16 @@
 import { makeStyles } from '@material-ui/core/styles';
+import { colors } from '../../colors';
 
 export const useNavStyles = makeStyles({
     navMenuLink: {
-        color: 'inherit',
+        color: colors.text.primary,
         textDecoration: 'none'
     },
     navAppBar: {
         width: '100vw',
-        marginBottom: '3vh'
+        marginBottom: '3vh',
+        background: colors.background.secondary,
+        boxShadow: '0px 0px 15px 2px rgba(0, 0, 0, 0.8)',
     },
     mobileTitle: {
         textAlign: 'center',
