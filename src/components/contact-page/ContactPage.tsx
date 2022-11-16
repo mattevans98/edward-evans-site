ab28cea5923dce6acedcf021d3b3e1302f3d2f8c
Matt Evans <mattevans98@gmail.com>
2022-11-16 14:16:36 -0600

    Consolidated shared CSS between pages into one file for <ContactPage />

--- src/components/contact-page/ContactPage.tsx
+++ src/components/contact-page/ContactPage.tsx
@@ -3,28 +3,30 @@ import NavMenu from "../common/NavMenu";
 import { ContactPageProps } from "./utils/ContactPage.model";
 import { Box, Typography, useTheme } from "@material-ui/core";
 import { useContactStyles } from '../../styles/components/ContactPage.style';
+import { useCommonStyles } from "../../styles/components/common/common.style";
 
 const ContactPage = (props: ContactPageProps): React.ReactElement => {
     const theme = useTheme();
+    const commonClasses = useCommonStyles(theme);
     const classes = useContactStyles(theme);
 
     return (
-        <Box className={ classes.mainBox }>
+        <Box className={ commonClasses.mainBox }>
             <NavMenu { ...props } />
             <Box>
-                <Typography variant="h1" className={ classes.contactH1 }>
+                <Typography variant="h1" className={ commonClasses.titleH1 }>
                     Contact
                 </Typography>
             </Box>
 
-            <Box className={ classes.contactContentBox }>
-                <Typography variant="body1" className={ classes.contactP }>
+            <Box className={ commonClasses.contentBox }>
+                <Typography variant="body1" className={ commonClasses.bodyText }>
                     If you would like to contact me in regards to any offers or questions, I can be reached through
                     email at <a href="mailto:matt@edwardevans.io" className={ classes.contactLink }>matt@edwardevans.io</a>
                 </Typography>
             </Box>
 
-            <Box className={ classes.contactFooter }>
+            <Box className={ commonClasses.footer }>
                 <Typography variant="body2">
                     Made with love and React by Matt Evans.
                     <br/>
